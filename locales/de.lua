522b26f488103ecf9f5ba839559005f12b32c8e2
NebelRebell <40825041+NebelRebell@users.noreply.github.com>
2019-04-03 02:28:50 +0200

    Create
    de.lua Translated from EN to DE

---
+++ locales/de.lua
@@ -0,0 +1,12 @@
+Locales['de'] = {
+  ['vehicle_robbery'] 			= 'Fahrzeugdiebstahl!',
+  ['mission_failed'] 			= 'Mission fehlgeschlagen!',
+  ['get_back_car_10s'] 			= 'Du hast 10 Sekunden, um wieder ins Auto zu steigen.',
+  ['get_back_car_1m'] 			= 'Du hast 1 Minute Zeit, um wieder ins Auto zu steigen.',
+  ['car_provided_rule'] 		= 'Sie müssen das für Sie bereitgestellte Auto benutzen und bis zum Anschlag fahren.',
+  ['already_robbery'] 			= 'Es ist bereits ein Autodiebstahl im Gange!',
+  ['not_enough_cops'] 			= 'Nicht genug Polizisten in der Stadt!',
+  ['car_stealing_in_progress'] 	= 'Autodiebstahl im Gange! Der Tracker wird auf Deiner Karte markiert.',
+  ['steal_a_car'] 				= 'Benutze ~INPUT_CONTEXT~ um ein Auto zu stehlen.',
+  ['drop_car_off'] 				= 'Benutze ~INPUT_CONTEXT~ um das Auto abzugeben.'
+}
